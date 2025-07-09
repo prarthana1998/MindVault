d3f06e8b730801a5afed5991ff09886d57b47d3c
prarthana1998 <12.prarthanakandwal@gmail.com>
2025-07-09 14:34:29 +0100

    Adding bot and shield protection using arcjet

--- middleware.js
+++ middleware.js
@@ -1,3 +1,4 @@
+import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -6,25 +7,38 @@ const isProtectedRoute = createRouteMatcher([
   "/journal(.*)",
   "/collection(.*)",
 ]);
- 
+
+// bot detection - arcjet
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE"]
+    })
+  ],
+});
 // auth will provide the user id and req will provide the page that the user is in
-export default clerkMiddleware(async (auth, req) => {
-  const { userId, redirectToSignIn } = await auth()
+const clerk =  clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
 
   if (!userId && isProtectedRoute(req)) {
     return redirectToSignIn();
-
   }
 
-  return NextResponse.next()
-
+  return NextResponse.next();
 });
+
+export default createMiddleware(aj, clerk)
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
