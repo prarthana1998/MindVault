809cc67347ccb6118f209e07ba8c32669d385e14
prarthana1998 <12.prarthanakandwal@gmail.com>
2025-07-15 17:29:01 +0100

    Formated the UI of the journal pages plus added new shadcn component

--- app/(main)/journal/[id]/page.jsx
+++ app/(main)/journal/[id]/page.jsx
@@ -1,23 +1,50 @@
-import { getJournalEntry } from '@/actions/journal';
-import React from 'react'
-import EditButton from './_components/editbutton';
-import DeleteDialog from './_components/deletedialog';
+import { getJournalEntry } from "@/actions/journal";
+import React from "react";
+import EditButton from "./_components/editbutton";
+import DeleteDialog from "./_components/deletedialog";
+import { getMoodById } from "@/app/lib/moods";
+import Link from "next/link";
+import { Badge } from "@/components/ui/badge";
 
-const JournalEntry = async({params}) => {
-    const {id} = await params;
-    const entry = await getJournalEntry(id)
-  return <div>
-    <div>
-        <div>
-            <h1>{entry.title}</h1>
-            {/* <p> </p> */}
+const JournalEntry = async ({ params }) => {
+  const { id } = await params;
+  const entry = await getJournalEntry(id);
+  const mood = getMoodById(entry.mood);
+  return (
+    <div className="p-6 space-y-6">
+      {/* Header */}
+
+      <div className="flex items-center justify-between">
+        {/* groups emoji and title */}
+        <div className="flex items-center gap-4">
+          {mood?.emoji && <span className="text-4xl">{mood.emoji}</span>}
+          <h1 className="text-5xl font-bold gradient-title pb-4">
+            {entry.title}
+          </h1>
         </div>
-        <div>
-            <EditButton/>
-            <DeleteDialog/>
+
+        <div className="flex gap-2">
+          <EditButton />
+          <DeleteDialog />
         </div>
+      </div>
+
+      <div
+        className="ql-editor prose max-w-none text-gray-800"
+        dangerouslySetInnerHTML={{ __html: entry.content }}
+      />
+
+      <div className="flex flex-wrap gap-2">
+        {entry.collection && (
+          <Link href={`/collection/${entry.collection.id}`}>
+            <Badge className="bg-brown-100 text-black-800 border border-black-300 hover:bg-black-200 transition">
+              Collection: {entry.collection.name}
+            </Badge>
+          </Link>
+        )}
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
-export default JournalEntry
+export default JournalEntry;
