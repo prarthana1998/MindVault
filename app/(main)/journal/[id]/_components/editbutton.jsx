d4c2c753dee0d967d113a9b07cb813a9b3f70eb4
prarthana1998 <12.prarthanakandwal@gmail.com>
2025-08-15 16:18:31 +0100

    Adding the editi button

--- app/(main)/journal/[id]/_components/editbutton.jsx
+++ app/(main)/journal/[id]/_components/editbutton.jsx
@@ -1,9 +1,17 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { Edit } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
-const EditButton = () => {
+const EditButton = ({entryId}) => {
+  const router = useRouter();
   return (
-    <div>EditButton</div>
+    <Button
+    variant="outline"
+    onClick={()=>router.push(`/journal/write?edit=${entryId}`)}><Edit className='h-4 w-4'/>
+    Edit
+    </Button>
   )
-}
-
-export default EditButton
+};
+export default EditButton;
