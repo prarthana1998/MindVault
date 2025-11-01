94706c9318b98517e29f22fe024cb13d828d2a37
prarthana1998 <12.prarthanakandwal@gmail.com>
2025-11-01 20:43:08 +0000

    Added animations on the home page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Book, Brain, ChevronRightIcon, Feather, FeatherIcon, Lock, Sparkles } from "lucide-react";
 import Link from "next/link";
+import {motion} from "framer-motion"
 const features = [
   {
     icon: Book,
@@ -26,10 +28,10 @@ export default function Home() {
       {/* Hero Section */}
       <div className="max-w-5xl mx-auto text-center space-y-8">
         <h1 className="text-4xl md:text-5xl lg:text-6xl gradient-title">
-        Preserve your thoughts in your personal MindVault
+          Preserve your thoughts in your personal MindVault
         </h1>
         <p className="text-lg md:text-xl text-brown-600">
-        A simple, elegant journal for clear writing, secure storage, and zero distractions.
+          A simple, elegant journal for clear writing, secure storage, and zero distractions.
         </p>
         <div className="flex justify-center gap-4">
           <Link href="/dashboard">
@@ -39,9 +41,9 @@ export default function Home() {
             </Button>
           </Link>
           <Link href="#features">
-          <Button size="lg" variant="outline" className="text-brown-600 rounded-full border-brown-400 hover:bg-brown-400/10">
-            Explore Features
-          </Button>
+            <Button size="lg" variant="outline" className="text-brown-600 rounded-full border-brown-400 hover:bg-brown-400/10">
+              Explore Features
+            </Button>
           </Link>
         </div>
       </div>
@@ -49,33 +51,41 @@ export default function Home() {
         {features.map((features, index) => (
           <Card key={features.title} className="bg-brown-100/50 p-4 rounded-lg backdrop-blur-sm border border-brown-200/20">
             <CardContent className="p-6">
-              <div className="h-12 w-12 bg-brown-100 rounded-full flex items-center justify-center mb-4"><features.icon className="h-8 w-8 text-brown-300"/></div>
+              <div className="h-12 w-12 bg-brown-100 rounded-full flex items-center justify-center mb-4"><features.icon className="h-8 w-8 text-brown-300" /></div>
               <h3 className="text-xl font-semibold text-brown-700">{features.title}</h3>
-          {/* Feature Description */}
-          <p className="text-sm text-brown-600 mt-2">{features.description}</p>
+              {/* Feature Description */}
+              <p className="text-sm text-brown-600 mt-2">{features.description}</p>
             </CardContent>
           </Card>
         ))}
       </section>
       <div className="container mx-auto px-4 py-24 ">
-     <Card className="bg-gradient-to-r from-brown-100 via-brown-200 to-brown-300 ">
-      <CardContent className="p-12 text-center">
-      {/* <Brain className="h-16 w-16 mx-auto mb-6 text-brown-300" /> */}
-        <h3 className="text-3xl font-bold text-brown-700 mb-6">
-          Start writing your daily thoughts here
-        </h3>
-        <p className="text-lg text-brown-700 mb-6">
-        Reflect, grow, and organize your thoughts. Your private space awaits.
-        </p>
-        <Link href="/dashboard">
-    <Button variant="journal" className="animate-bounce">
-      <FeatherIcon className="h-5 w-5" />Start Journaling for free 
-    </Button></Link>
-      </CardContent>
-     </Card>
-     </div>
+        <motion.div
+          initial={{ x: -100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          viewport={{ once: true, amount: 0.1 }}
+          transition={{ duration: 0.6, ease: "easeOut", delay: 0.2 }}
+          className="max-w-3xl mx-auto"
+        >
+        <Card className="bg-gradient-to-r from-brown-100 via-brown-200 to-brown-300 ">
+          <CardContent className="p-12 text-center">
+
+            <h3 className="text-3xl font-bold text-brown-700 mb-6">
+              Start writing your daily thoughts here
+            </h3>
+            <p className="text-lg text-brown-700 mb-6">
+              Reflect, grow, and organize your thoughts. Your private space awaits.
+            </p>
+            <Link href="/dashboard">
+              <Button variant="journal" className="animate-bounce">
+                <FeatherIcon className="h-5 w-5" />Start Journaling for free
+              </Button></Link>
+          </CardContent>
+        </Card>
+        </motion.div>
+      </div>
     </div>
-    
+
 
   );
 }
